0881a68112cbc25262dadedd4b24dd752e93a212
Ben Armston <ben.armston@alces-software.com>
2020-05-15 11:36:16 +0100

    Simplify loading of apps

--- www/lib/helpers.rb
+++ www/lib/helpers.rb
@@ -1,7 +1,17 @@
 use_helper Nanoc::Helpers::Rendering
 
 def installed_apps
-  @_installed_apps ||= items.select { |i| i[:kind] == 'app' }
+  @_installed_apps ||= items
+    .select { |i| i[:kind] == 'app' }
+    .sort { |a, b|
+      if a[:order] && b[:order]
+        [ a[:order], a.identifier ] <=> [ b[:order], b.identifier ]
+      elsif a[:order].nil? && b[:order].nil?
+        a.identifier <=> b.identifier
+      else
+        a[:order] ? -1 : 1
+      end
+    }
 end
 
 def environment
