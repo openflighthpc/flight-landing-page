7097fc038faa2e2aa7b5520bb56bdb3ac0d0937c
William McCumsite <william.mccumstie@gmail.com>
2021-06-08 15:24:51 +0100

    Remove the disabled metadata tag

--- landing-page/lib/helpers.rb
+++ landing-page/lib/helpers.rb
@@ -68,7 +68,7 @@ end
 def app_items
   @_apps ||= items
     .find_all('/apps/**/*')
-    .reject { |i| /\.disabled\Z/.match?(i.identifier) || i[:disabled] }
+    .reject { |i| /\.disabled\Z/.match?(i.identifier) }
     .sort { |a, b|
       if a[:order] && b[:order]
         [ a[:order], a.identifier ] <=> [ b[:order], b.identifier ]
@@ -86,7 +86,7 @@ end
 
 def config_pack_items
   @__config_packs ||= items.find_all('/config-packs/**/*')
-                           .reject { |i| /\.disabled\Z/.match?(i.identifier) || i[:disabled] }
+                           .reject { |i| /\.disabled\Z/.match?(i.identifier) }
                            .sort do |a, b|
     if a[:order] && b[:order]
       [ a[:order], a.identifier ] <=> [ b[:order], b.identifier ]
